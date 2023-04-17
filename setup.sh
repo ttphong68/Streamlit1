1deff35a3163b23702646278990234d05cdbc099
ttphong68 <106058535+ttphong68@users.noreply.github.com>
2023-04-18 05:31:49 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ email = \"ttphong68@gmail.com\"\n\
 echo "\
 [server]\n\
 headless = true\n\
+audio = portaudio\n\
 enableCORS=false\n\
+enableXsrfProtection = false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
